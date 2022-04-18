dfb82539b4698dd43a4dccaee7e27d2daeb321f8
Ananya <anta1515@colorado.edu>
2022-04-18 11:16:19 -0600

    js debug

--- js/assignment13.js
+++ js/assignment13.js
@@ -34,7 +34,7 @@ function result() {
     // const weight = Math.round(300*0.0714286) stone`;
     // const temperature =  `${Math.round((94-32) * 5 / 9)} centigrade`;
     const weight = '${Math.round(300*0.0714286)} stone';
-    const temperature =  '${Math.round((94-32) * 5 / 9)} centigrade';
+    const temperature =  `${Math.round((94-32) * 5 / 9)} centigrade`;
     newStory = newStory.replaceAll('94 fahrenheit', temperature);
     newStory = newStory.replaceAll('300 pounds', weight);
   }
